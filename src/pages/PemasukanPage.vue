a8109d37978a2eab40453e01d253c2577a810e6a
citraa28 <citraamellia43@gmail.com>
2025-08-07 14:59:09 +0700

    mengganti icon halaman pemasukan

--- src/pages/PemasukanPage.vue
+++ src/pages/PemasukanPage.vue
@@ -6,20 +6,11 @@
         <h1
           class="text-3xl font-bold font-inter pb-1/ \ text-gray-800 flex items-center gap-2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-8 h-8"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
-            />
-          </svg>
+          <img
+            src="../assets/pemasukan.gif"
+            alt="Icon pemasukan"
+            class="w-20 h-20 object-contain"
+          />
           Pemasukan Uang
         </h1>
         <p><i>Ayo Kendalikan Uangmu</i></p>
