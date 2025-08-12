b8bc478e9c16c7fe69d3680fffa7ff292cfcf403
citraa28 <citraamellia43@gmail.com>
2025-08-12 18:27:19 +0700

    menghilangkan footer

--- src/pages/PengeluaranPage.vue
+++ src/pages/PengeluaranPage.vue
@@ -214,17 +214,6 @@
         @confirm="confirmDelete"
         @close="closeDeleteConfirmation"
       />
-      <footer>
-        <div class="mt-7 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-          <div class="sm:flex sm:items-center sm:justify-between">
-            <p
-              class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right"
-            >
-              Copyright &copy; 2025 CitraMaya. All rights reserved.
-            </p>
-          </div>
-        </div>
-      </footer>
     </div>
   </LayoutPage>
 </template>
