2547adde9ef9cb1a49d0c77551f8f3549d83058b
citraa28 <citraamellia43@gmail.com>
2025-08-27 14:46:43 +0700

    perubahan sedikit

--- src/pages/BerandaPage.vue
+++ src/pages/BerandaPage.vue
@@ -185,13 +185,15 @@
         </div>
       </div>
 
-      <div class="mt-10 mb-15 text-center text-gray-800 dark:text-black text-lg">
-        Mengelola keuangan bukan hanya tentang mencatat pemasukan dan pengeluaran, tapi
-        tentang membangun kebiasaan yang membawa kita lebih dekat pada stabilitas dan
-        impian finansial. Dengan langkah kecil yang konsisten, kamu sudah selangkah lebih
-        maju menuju masa depan yang lebih aman dan terencana. Terus lanjutkan, karena
-        setiap rupiah yang kamu kelola hari ini akan menentukan kenyamanan hidupmu esok
-        hari. 💰📈
+      <div
+        class="mt-10 mb-15 text-center text-gray-800 dark:text-black text-lg"
+      >
+        Mengelola keuangan bukan hanya tentang mencatat pemasukan dan
+        pengeluaran, tapi tentang membangun kebiasaan yang membawa kita lebih
+        dekat pada stabilitas dan impian finansial. Dengan langkah kecil yang
+        konsisten, kamu sudah selangkah lebih maju menuju masa depan yang lebih
+        aman dan terencana. Terus lanjutkan, karena setiap rupiah yang kamu
+        kelola hari ini akan menentukan kenyamanan hidupmu esok hari. 💰📈
       </div>
 
       <section class="bg-blue-200 rounded-lg py-16 px-4">
@@ -203,7 +205,9 @@
 
         <div class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
           <!-- Card 1 -->
-          <div class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative">
+          <div
+            class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative"
+          >
             <div class="absolute -top-8 left-1/2 transform -translate-x-1/2">
               <div class="bg-white p-3 rounded-xl shadow-md">
                 <img
@@ -214,7 +218,9 @@
               </div>
             </div>
             <div class="pt-12">
-              <h3 class="text-xl font-bold text-green-400 mb-2">Tujuan Keuangan</h3>
+              <h3 class="text-xl font-bold text-green-400 mb-2">
+                Tujuan Keuangan
+              </h3>
               <p class="text-sm">
                 Ambil aksi untuk gapai mimpi.<br />
                 Wujudkan impian dengan jangka waktu nabung yang kamu tentukan.
@@ -223,7 +229,9 @@
           </div>
 
           <!-- Card 2 -->
-          <div class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative">
+          <div
+            class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative"
+          >
             <div class="absolute -top-8 left-1/2 transform -translate-x-1/2">
               <div class="bg-white p-3 rounded-xl shadow-md">
                 <img
@@ -234,16 +242,20 @@
               </div>
             </div>
             <div class="pt-12">
-              <h3 class="text-xl font-bold text-green-400 mb-2">Catat Transaksi</h3>
+              <h3 class="text-xl font-bold text-green-400 mb-2">
+                Catat Transaksi
+              </h3>
               <p class="text-sm">
-                Otomatis atau manual, catat tiap pengeluaran untuk hari-hari anti boncos
-                mendatang.
+                Otomatis atau manual, catat tiap pengeluaran untuk hari-hari
+                anti boncos mendatang.
               </p>
             </div>
           </div>
 
           <!-- Card 3 -->
-          <div class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative">
+          <div
+            class="bg-blue-700 text-white p-6 rounded-2xl shadow-md relative"
+          >
             <div class="absolute -top-8 left-1/2 transform -translate-x-1/2">
               <div class="bg-white p-3 rounded-xl shadow-md">
                 <img
@@ -258,7 +270,8 @@
               <h3 class="text-xl font-bold text-green-400 mb-2">Budgeting</h3>
               <p class="text-sm">
                 Semua ada budgetnya, kok!<br />
-                Atur anggaran keuangan kamu untuk segala kebutuhan bareng Sribuu.
+                Atur anggaran keuangan kamu untuk segala kebutuhan bareng
+                Sribuu.
               </p>
             </div>
           </div>
@@ -281,8 +294,8 @@
             Cerdas berfinansial dengan rekomendasi pintar
           </h1>
           <p class="text-gray-500 text-lg">
-            Dapatkan insight dan rekomendasi bulanan yang bisa ngasih kamu arahan biar
-            lebih bijak dalam ngatur uang.
+            Dapatkan insight dan rekomendasi bulanan yang bisa ngasih kamu
+            arahan biar lebih bijak dalam ngatur uang.
           </p>
         </div>
       </div>
@@ -290,7 +303,7 @@
       <div class="md:justify-center mt-10">
         <!-- Judul -->
         <h1
-          class="text-3xl text-center md:text-4xl font-serif font-bold text-gray-700 mb-10"
+          class="text-3xl text-center md:text-4xl font-serif font-bold text-gray-700 mb-15"
         >
           Ringkas dan Rapi dengan<br />
           Website Keuangan MAYTRA Finance
@@ -305,9 +318,12 @@
               alt="Ready"
               class="w-14 md:w-23 h-auto object-contain mb-4"
             />
-            <h2 class="text-xl md:text-2xl font-bold text-gray-700">Siap Pakai</h2>
+            <h2 class="text-xl md:text-2xl font-bold text-gray-700">
+              Siap Pakai
+            </h2>
             <p class="text-base md:text-lg text-gray-700">
-              Tanpa perlu buat program dari awal, tinggal daftar dan langsung gunakan
+              Tanpa perlu buat program dari awal, tinggal daftar dan langsung
+              gunakan
             </p>
           </div>
 
@@ -322,8 +338,8 @@
               Gratis dan Praktis
             </h2>
             <p class="text-base md:text-lg text-gray-700">
-              Bebas biaya, pencatatan otomatis terintegrasi dengan setiap invoice
-              pembelian dan penjualan di Website MAYTRA Finance
+              Bebas biaya, pencatatan otomatis terintegrasi dengan setiap
+              invoice pembelian dan penjualan di Website MAYTRA Finance
             </p>
           </div>
 
@@ -338,7 +354,8 @@
               Cocok Digunakan untuk Pemula
             </h2>
             <p class="text-base md:text-lg text-gray-700">
-              Website yang simple, tapi sangat bermanfaat dan mudah digunakan untuk pemula
+              Website yang simple, tapi sangat bermanfaat dan mudah digunakan
+              untuk pemula
             </p>
           </div>
 
@@ -572,7 +589,9 @@ onMounted(() => {
     kategoriPengeluaranMap[item.kategori] += Number(item.harga);
   });
 
-  dataPieChartPengeluaran.value.data.labels = Object.keys(kategoriPengeluaranMap);
+  dataPieChartPengeluaran.value.data.labels = Object.keys(
+    kategoriPengeluaranMap
+  );
   dataPieChartPengeluaran.value.data.datasets = [
     {
       data: Object.values(kategoriPengeluaranMap),
@@ -597,7 +616,8 @@ const totalPemasukanBulanIni = computed(() => {
     .filter((item) => {
       const date = new Date(item.tanggal);
       return (
-        date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()
+        date.getMonth() === now.getMonth() &&
+        date.getFullYear() === now.getFullYear()
       );
     })
     .reduce((total, item) => total + Number(item.harga), 0);
@@ -610,7 +630,8 @@ const totalPengeluaranBulanIni = computed(() => {
     .filter((item) => {
       const date = new Date(item.tanggal);
       return (
-        date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()
+        date.getMonth() === now.getMonth() &&
+        date.getFullYear() === now.getFullYear()
       );
     })
     .reduce((total, item) => total + Number(item.harga), 0);
